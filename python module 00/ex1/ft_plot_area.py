d99af16bb27a19e785523aaf0960efdbf6cb18ae
M677871 <miledaz1234@gmail.com>
2026-01-20 11:04:32 +0200

    starting with modules 01

--- python module 00/ex1/ft_plot_area.py
+++ python module 00/ex1/ft_plot_area.py
@@ -1,5 +1,5 @@
 def ft_plot_area() -> None:
-    lengh = int(input("Enter lenght: "))
+    lengh = int(input("Enter length: "))
     width = int(input("Enter width: "))
     area = lengh * width
     print(f"Plot area: {area}")
