a01c5895661e4278ef71a3cd3dfee34d95730cc1
M677871 <miledaz1234@gmail.com>
2026-01-22 11:31:08 +0200

    fixing errors

--- python module 01/ex2/ft_plant_growth.py
+++ python module 01/ex2/ft_plant_growth.py
@@ -1,10 +1,16 @@
 class Plant:
-    def __init__(self, name, height, age):
+    def __init__(self, name: str, height: int, age: int) -> None:
         self.name = name
         self.height = height
         self.age = age
 
+    def grow(self, height_increase: int) -> None:
+        self.height += height_increase
+    
+    def age(self, days: int) -> None:
+        self.age += days
 
-    def get_info(self):
-        print(f"{self.name}: {self.height}cm, {self.age} days old")
-        
+    def get_info(self) -> str:
+        return f"{self.name}: {self.height}cm, {self.age} days old"
+
+
