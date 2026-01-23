9c07dcd07840fd556a34f2d63e639e28533d5466
M677871 <miledaz1234@gmail.com>
2026-01-23 19:16:06 +0200

    ex5

--- python module 01/ex5/ft_plant_types.py
+++ python module 01/ex5/ft_plant_types.py
@@ -44,3 +44,39 @@ class Flower(Plant):
     def bloom(self) -> None:
         print(f"{self._name} is blooming beautifully")
 
+class Tree(Plant):
+    def __init__(self, name: str, height: int, age: int, trunk_diameter: int) -> None:
+        super().__init__(name,height,age)
+        self.trunk_diameter = trunk_diameter
+
+    @property
+    def trunk_diameter(self) -> int:
+        return self._trunk_diameter
+
+    @trunk_diameter.setter
+    def trunk_diameter(self, trunk_diameter: int) -> None:
+        if trunk_diameter < 0:
+            print("the truck diamter should be positive")
+            self._trunk_diameter = 0
+        else:
+            self._trunk_diameter = trunk_diameter
+
+    def produce_shade(self) -> None:
+        shade = self.trunk_diameter * 1.56
+        print(f"{self.name} provides {shade} square meters of shade")
+
+class Vegetable(Plant):
+    def __init__(self, name: str, height: int, age: int,
+                 harvest_season: str, nutritional_value: str) -> None:
+        super().__init__(name, height, age)
+        self.harvest_season = harvest_season
+        self.nutrional_value = nutritional_value
+
+    def info(self) -> None:
+         print(f"{self.name} is rich in {self.nutritional_value}")
+
+def main() -> None:
+    print("=== Plant Types Test ===")
+    # rose = Flower("Rose", 25, 30, "Red")
+    # oak = Tree("Oak", 200, 365, 50)
+    # carrot = Vegetable("Carrot", 15, 120, "Fall", "Vitamin A")
