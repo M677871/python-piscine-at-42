ddeb8a546e3438584a98d4953c9c08625fdd8748
Miled issa <miissa@student.42beirut.com>
2026-02-06 14:25:25 +0200

    fixing an error in ex3 that is catched during the evaluation

--- python module 01/ex3/ft_plant_factory.py
+++ python module 01/ex3/ft_plant_factory.py
@@ -8,15 +8,25 @@ class Plant:
         return f"{self.name}: ({self.height}cm, {self.age_days} days)"
 
 
+def generate_plants(count: int) -> list[Plant]:
+    p1: tuple[str, int, int] = ("Rose", 25, 30)
+    p2: tuple[str, int, int] = ("Oak", 200, 365)
+    p3: tuple[str, int, int] = ("Cactus", 5, 90)
+    p4: tuple[str, int, int] = ("Sunflower", 80, 45)
+    p5: tuple[str, int, int] = ("Fern", 15, 120)
+    plants: list[tuple[str, int, int]] = [p1, p2, p3, p4, p5]
+    p: list[Plant] = [None] * count
+    for i in range(count):
+        name: str = plants[i % 5][0]
+        height: int = plants[i % 5][1] + (i % count)
+        age: int = plants[i % 5][2] + (7 + i % count)
+        p[i] = Plant(name, height, age)
+    return p
+
+
 def main() -> None:
     print("=== Plant Factory Output ===")
-    rose = Plant("Rose", 25, 30)
-    oak = Plant("Oak", 200, 365)
-    cactus = Plant("Cactus", 5, 90)
-    sunflower = Plant("Sunflower", 80, 45)
-    fern = Plant("Fern", 15, 120)
-
-    plants: list[Plant] = [rose, oak, cactus, sunflower, fern]
+    plants: list[Plant] = generate_plants(20)
     total_plants = 0
     for plant in plants:
         print(f"Created: {plant.get_info()}")
